d88a50e35c35da5102bd4580727d12b8cc8f256b
Uri <oriol.roca01@estudiant.upf.edu>
2024-04-17 12:25:50 +0200

    first config v2

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return jsonify(message="Hello, World!")
+def serve():
+    return send_from_directory(os.path.join(os.getcwd(), 'frontend', 'build'), 'index.html')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory(os.path.join(os.getcwd(), 'frontend', 'build', 'static'), path)
 
 if __name__ == '__main__':
     app.run(debug=True)
