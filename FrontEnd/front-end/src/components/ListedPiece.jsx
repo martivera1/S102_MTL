43a923b076f7eb8bb3eb86a6767fdb42578465fb
srhoms <ferranh2@gmail.com>
2024-04-17 21:37:55 +0200

    Created the folder front-end that includes all the react project

---
+++ FrontEnd/front-end/src/components/ListedPiece.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image1 from '../images/pdf_image.png'
+import Image2 from '../images/url_image.png'
+
+const ListedPiece = ({title, type, size}) => {
+  return (
+    <div className='flex flex-row my-4 ml-6'>
+      <div className='flex mr-4'>
+        {type === 'pdf' ? <img src={Image1} alt='imageNotFound' className='size-14'></img> : <img src={Image2} alt='imageNotFound' className='size-10'></img>}
+      </div>
+      <div className='flex flex-col'>
+        <p className='font-roboto font-medium'>{title}</p>
+        <p className='font-roboto text-sm'>{size}</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default ListedPiece
