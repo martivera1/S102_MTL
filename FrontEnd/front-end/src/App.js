58e741e7984746f3d70c1e18a6cf1f82bccbe6fe
Uri <oriol.roca01@estudiant.upf.edu>
2024-04-24 11:15:30 +0200

    flask API config v2

--- FrontEnd/front-end/src/App.js
+++ FrontEnd/front-end/src/App.js
@@ -1,51 +1,25 @@
-import React, { useEffect, useState } from "react"
-import "./App.css"
-import Card from "./components/Card"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import "./static/styles/App.css";
 
 function App() {
-    const [homeMessage, setHomeMessage] = useState("")
-    const [uploadMessage, setUploadMessage] = useState("")
-    const [error, setError] = useState("")
-
-    // Fetch data for /home endpoint
-    useEffect(() => {
-        fetch("http://localhost:5000/home")
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok")
-                }
-                return response.json()
-            })
-            .then((data) => setHomeMessage(data.message))
-            .catch((error) => setError(error.message))
-    }, [])
-
-    // Fetch data for /upload endpoint
-    useEffect(() => {
-        fetch("http://localhost:5000/upload", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({}),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok")
-                }
-                return response.json()
-            })
-            .then((data) => setUploadMessage(data.message))
-            .catch((error) => setError(error.message))
-    }, [])
-
     return (
-        <div>
-            <h1>/home Message: {homeMessage}</h1>
-            <h1>/upload Message: {uploadMessage}</h1>
-            {error && <p>Error: {error}</p>}
-        </div>
-    )
+        <Router>
+            <div>
+                {/* Header Component Here */}
+                
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/upload" element={<Upload />} />
+                    {/* Add other routes here */}
+                </Routes>
+                
+                {/* Footer Component Here */}
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
