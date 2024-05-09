12b8c59d36f98f380f30632084b23c483d5e5524
oriolroca01 <92043637+oriolroca01@users.noreply.github.com>
2024-05-09 19:53:46 +0200

    Update app.py

--- BackEnd/api/app.py
+++ BackEnd/api/app.py
@@ -1,7 +1,29 @@
 from flask import Flask, jsonify, g
 import mysql.connector
+import json
+from functools import wraps
+from config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
 
 app = Flask(__name__)
+app.secret_key = 'pianoclassification_app_key'
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+
+oauth = OAuth(app)
+google = oauth.remote_app(
+    'google',
+    consumer_key=GOOGLE_CLIENT_ID,
+    consumer_secret=GOOGLE_CLIENT_SECRET,
+    request_token_params={
+        'scope': 'email'
+    },
+    base_url='https://www.googleapis.com/oauth2/v1/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
 
 def get_db():
     if 'db' not in g:
