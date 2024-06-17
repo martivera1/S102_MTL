e8885f13b0f246ae478c49c523b69a878fe95fe7
FerranHoms <ferran.homs@estudiant.upf.edu>
2024-06-17 19:10:31 +0200

    yea

--- FrontEnd/front-end/src/pages/Upload.js
+++ FrontEnd/front-end/src/pages/Upload.js
@@ -3,33 +3,11 @@ import Card from "../components/Card";
 import { BACKEND_URL } from "../constants";
 
 function Upload() {
-    const [pieces, setPieces] = useState([]);
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        fetch(`${BACKEND_URL}/get_links`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                return response.json();
-            })
-            .then((data) => setPieces(data.links.map((link, index) => ({ ...link, id: String(index) }))))
-            .catch((error) => setError(error.message));
-    }, []);
-
-    const updatePieceStatus = (link, status) => {
-        setPieces((prevPieces) => 
-            prevPieces.map((piece) =>
-                piece.link === link ? { ...piece, status: status } : piece
-            )
-        );
-    };
 
     return (
         <div className="flex justify-center items-center">
             <div className="w-full">
-                <Card pieces={pieces} updatePieceStatus={updatePieceStatus} />
+                <Card/>
             </div>
         </div>
     );
